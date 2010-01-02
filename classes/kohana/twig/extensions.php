759c005612241517ccf12b336a52fe3c453e3c15
Jonathan Geiger <jonathan@jonathan-geiger.com>
2010-01-02 11:00:37 -0500

    Adding a few filters

--- classes/kohana/twig/extensions.php
+++ classes/kohana/twig/extensions.php
@@ -10,6 +10,35 @@ class Kohana_Twig_Extensions extends Twig_Extension
 			new Kohana_Twig_Asset_Stylesheet_TokenParser(),
 		);
 	}
+	
+	public function getFilters()
+	{
+		return array(
+			// Date and time
+			'timestamp' => new Twig_Filter_Function('strtotime'),
+			'timesince' => new Twig_Filter_Function('Kohana_Twig_Filters::timesince'),
+			'fuzzy_timesince' => new Twig_Filter_Function('date::fuzzy_span'),
+			
+			// Strings
+			'plural' => new Twig_Filter_Function('inflector::plural'),
+			'singular' => new Twig_Filter_Function('inflector::singular'),
+			'humanize' => new Twig_Filter_Function('inflector::humanize'),
+			
+			// HTML 
+			'obfuscate' => new Twig_Filter_Function('html::obfuscate'),
+			
+			// Numbers
+			'ordinal' => new Twig_Filter_Function('Kohana_Twig_Filters::ordinal'),
+			'num_format' => new Twig_Filter_Function('num::format'),
+			
+			// Text
+			'limit_words' => new Twig_Filter_Function('text::limit_words'),
+			'limit_chars' => new Twig_Filter_Function('text::limit_chars'),
+			'auto_link' => new Twig_Filter_Function('text::auto_link'),
+			'auto_p' => new Twig_Filter_Function('text::auto_p'),
+			'bytes' => new Twig_Filter_Function('text::bytes'),
+		);
+	}
 
 	public function getName()
 	{
---
+++ classes/kohana/twig/filters.php
@@ -0,0 +1,68 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kohana_Twig_Filters
+{
+	public static function ordinal($number)
+	{
+		return $number.num::ordinal($number);
+	}
+	
+	public static function timesince($older_date, $newer_date = NULL)
+	{
+		// array of time period chunks
+		$chunks = array(
+			array(60 * 60 * 24 * 365 , 'year'),
+			array(60 * 60 * 24 * 30 , 'month'),
+			array(60 * 60 * 24 * 7, 'week'),
+			array(60 * 60 * 24 , 'day'),
+			array(60 * 60 , 'hour'),
+			array(60 , 'minute'),
+		);
+		
+		// Convert to a unix timestamp
+		$older_date = strtotime($older_date);
+
+		// $newer_date will equal false if we want to know the time elapsed between a date and the current time
+		// $newer_date will have a value if we want to work out time elapsed between two known dates
+		$newer_date = ($newer_date == false) ? time() : strtotime($newer_date);
+
+		// difference in seconds
+		$since = $newer_date - $older_date;
+
+		// we only want to output two chunks of time here, eg:
+		// x years, xx months
+		// x days, xx hours
+		// so there's only two bits of calculation below:
+
+		// step one: the first chunk
+		for ($i = 0, $j = count($chunks); $i < $j; $i++)
+			{
+			$seconds = $chunks[$i][0];
+			$name = $chunks[$i][1];
+
+			// finding the biggest chunk (if the chunk fits, break)
+			if (($count = floor($since / $seconds)) != 0)
+			{
+				break;
+			}
+		}
+
+		// set output var
+		$output = ($count == 1) ? '1 '.$name : "$count {$name}s";
+
+		// step two: the second chunk
+		if ($i + 1 < $j)
+			{
+			$seconds2 = $chunks[$i + 1][0];
+			$name2 = $chunks[$i + 1][1];
+
+			if (($count2 = floor(($since - ($seconds * $count)) / $seconds2)) != 0)
+			{
+				// add to output var
+				$output .= ($count2 == 1) ? ', 1 '.$name2 : ", $count2 {$name2}s";
+			}
+		}
+
+		return $output;
+	}
+}

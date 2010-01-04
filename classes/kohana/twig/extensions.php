2b69966cf5cf25c7e74eb2f183170a21555ad577
Jonathan Geiger <jonathan@jonathan-geiger.com>
2010-01-03 20:41:40 -0500

    Adding a partial tag

--- classes/kohana/twig/extensions.php
+++ classes/kohana/twig/extensions.php
@@ -7,6 +7,7 @@ class Kohana_Twig_Extensions extends Twig_Extension
 		return array(
 			new Kohana_Twig_URL_TokenParser(),
 			new Kohana_Twig_Cache_TokenParser(),
+			new Kohana_Twig_Partial_TokenParser(),
 			new Kohana_Twig_Asset_Javascript_TokenParser(),
 			new Kohana_Twig_Asset_Stylesheet_TokenParser(),
 		);

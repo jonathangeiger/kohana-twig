594dc426676a55a4d92f767431ef694cad077b58
Jonathan Geiger <jonathan@jonathan-geiger.com>
2010-01-04 22:48:55 -0500

    Updating the base controller

--- classes/controller/twig/template.php
+++ classes/controller/twig/template.php
@@ -5,47 +5,76 @@
  * @package    Kohana-Twig
  * @author     John Heathco <jheathco@gmail.com>
  */
-abstract class Controller_Twig extends Controller {
-
+abstract class Controller_Twig_Template extends Controller 
+{
 	/**
 	 * @var Twig_Environment
 	 */
-	public $twig;
+	protected $twig;
 
 	/**
 	 * @var boolean  Auto-render template after controller method returns
 	 */
-	public $auto_render = TRUE;
+	protected $auto_render = TRUE;
 
 	/**
 	 * @var array|object  Stores mapping of template vars => values
 	 */
-	public $context;
+	protected $context;
+	
+	/**
+	 * @var string The template to render
+	 */
+	protected $template;
 
+	/**
+	 * Constructor
+	 *
+	 * @param Request $request 
+	 * @author Jonathan Geiger
+	 */
 	public function __construct(Request $request)
 	{
 		// Setup the Twig loader environment
 		$this->twig = Kohana_Twig::instance();
+		
+		// Search for a default context
+		$config = Kohana::config('context');
+		
+		// Search for a default context based on the URI
+		if (isset($config[$request->uri]))
+		{
+			$this->context = $config[$request->uri];
+		}
+		// Try based on controller/action
+		else if (isset($config[$request->controller.'/'.$request->action]))
+		{
+			$this->context = $config[$request->controller.'/'.$request->action];
+		}
 
+		// Ensure it's cast to the proper type
 		if (Kohana_Twig::$config->context_object)
 		{
 			// Context treated as an object
-			$this->context = new stdClass;
+			$this->context = (object)$this->context;
 		}
 		else
 		{
 			// Context treated as an array
-			$this->context = array();
+			$this->context = (array)$this->context;
 		}
 
 		// Auto-generate template filename ('index' method called on Controller_Admin_Users looks for 'admin/users/index')
 		$this->template = $request->controller.'/'.$request->action.Kohana_Twig::$config->suffix;
 
-		if ( ! empty($request->directory))
+		// Prepend directory if needed
+		if ( !empty($request->directory))
 		{
-			// Preprend directory if needed
 			$this->template = $request->directory.'/'.$this->template;
 		}
+		
+		// Convert underscores to slashes
+		$this->template = str_replace('_', '/', $this->template);
 
 		parent::__construct($request);
 	}

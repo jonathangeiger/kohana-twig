5e9fa06e026eaf9dfc036d1b440faded289f00ed
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-05-16 22:55:27 +0100

    Removed useless code in after method (IMHO) and add call to parent.

--- classes/kohana/controller/twig/template.php
+++ classes/kohana/controller/twig/template.php
@@ -62,21 +62,11 @@ abstract class Kohana_Controller_Twig_Template extends Controller
 	{	
 		if ($this->auto_render)
 		{
-			// Search for a default context
-			$config = Kohana::config('context');
-			
-			if (isset($config[$this->request->uri]))
-			{
-				$this->template->set_default($config[$this->request->uri]);
-			}
-			else if (isset($config[$this->template->path()]))
-			{
-				$this->template->set_default($config[$this->template->path()]);
-			}
-			
 			// Auto-render the template
 			$this->request->response = $this->template->render();
 		}
+
+		parent::after();
 	}
 
 } // End Controller_Twig

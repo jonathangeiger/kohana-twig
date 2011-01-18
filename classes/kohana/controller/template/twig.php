5e6767b2390533ab91026d8e0df1453196b7c531
Mathew Davies <thepixeldeveloper@googlemail.com>
2011-01-18 17:22:35 +0000

    3.1 compatibility changes.

--- classes/kohana/controller/template/twig.php
+++ classes/kohana/controller/template/twig.php
@@ -32,11 +32,13 @@ abstract class Kohana_Controller_Template_Twig extends Controller
 		if (empty($this->template))
 		{
 			// Generate a template name if one wasn't set.
-			$this->template = str_replace('_', DIRECTORY_SEPARATOR, $this->request->controller).DIRECTORY_SEPARATOR.$this->request->action;
+			$this->template = str_replace('_', DIRECTORY_SEPARATOR, $this->request->controller()).DIRECTORY_SEPARATOR.$this->request->action();
 
-			if ( ! empty($this->request->directory))
+			$directory = $this->request->directory();
+			
+			if ( ! empty($directory))
 			{
-				$this->template = $this->request->directory.DIRECTORY_SEPARATOR.$this->template;
+				$this->template = $this->request->directory().DIRECTORY_SEPARATOR.$this->template;
 			}
 		}
 
@@ -62,7 +64,7 @@ abstract class Kohana_Controller_Template_Twig extends Controller
 		if ($this->auto_render)
 		{
 			// Auto-render the template
-			$this->request->response = $this->template;
+			$this->response->body($this->template);
 		}
 
 		return parent::after();
